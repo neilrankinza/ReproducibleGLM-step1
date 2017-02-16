ea5761947d3248abe4a5cb38de12dd0600fd0d3c
Steph Locke <steph@itsalocke.com>
2017-02-16 09:00:47 +0000

    Revised packrat method

--- build.R
+++ build.R
@@ -1,5 +1,3 @@
 packrat::on()
-packrat::restore()
-packrat::status()
 rmarkdown::render("README.Rmd", output_format = rmarkdown::github_document(), output_dir="docs")
 rmarkdown::render("outputs/FullModel.Rmd", output_dir="docs")
