3c6578ce499fcf837c58f9cb68de78c8b372b549
Steph Locke <steph@itsalocke.com>
2017-02-16 09:05:03 +0000

    In situ

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,11 @@ MODELVERS="$(sed -n "s/Version: *\([^ ]*\)/\1/p" DESCRIPTION)"
 MODELSRC="$(basename `pwd`)"
 AUTHORNAME="Steph"
 AUTHOREMAIL="Steph@itsalocke.com"
-RENDERDIR="../new_version"
 GITURL="https://$GITHUB_PAT@github.com/$TRAVIS_REPO_SLUG.git"
 
-git clone $GITURL $RENDERDIR
+
 git config --global user.name $AUTHORNAME
 git config --global user.email $AUTHOREMAIL
-cd $RENDERDIR
 
 R CMD BATCH build.R
 cp build.Rout docs
