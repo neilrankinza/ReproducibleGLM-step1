08b43a1a942e7342bb4f77606120c5726dc0f57e
Steph Locke <steph@itsalocke.com>
2017-02-16 09:55:51 +0000

    Wrong command!

--- build.R
+++ build.R
@@ -1,4 +1,4 @@
 sessionInfo()
-available.packages()
+installed.packages()
 rmarkdown::render("README.Rmd", output_format = rmarkdown::github_document(), output_dir="docs")
 rmarkdown::render("outputs/FullModel.Rmd", output_dir="docs")
