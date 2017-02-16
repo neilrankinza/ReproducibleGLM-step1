bcbea24fc6c19b697de794efc8ff62384d2a8889
Steph Locke <steph@itsalocke.com>
2017-02-16 10:03:38 +0000

    packrat on?

--- build.R
+++ build.R
@@ -1,4 +1,5 @@
 sessionInfo()
+packrat::on()
 installed.packages()
 rmarkdown::render("README.Rmd", output_format = rmarkdown::github_document(), output_dir="docs")
 rmarkdown::render("outputs/FullModel.Rmd", output_dir="docs")
